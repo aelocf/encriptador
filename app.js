2a3feb2457d07e83fe475773faa180b37f8abc17
aelocf <aelo.chamul@gmail.com>
2024-01-23 15:08:01 -0600

    se creo el codigo inicias js
    se crearon variables y bucles para encriptar y desenciptar dados

---
+++ app.js
@@ -0,0 +1,56 @@
+let palabra="hola a todos";
+//convertir un string en array
+let arreglo= palabra.split('');
+
+let palabraSecreta=arreglo;
+//convertimos un array en string
+let desencriptado= palabraSecreta.toString(arreglo).replace(/,/g, '');
+console.log(arreglo);
+console.log(desencriptado);
+
+for (let i = 0; i < arreglo.length; i++) {
+    if(arreglo[i]=="a"){
+        arreglo[i]="ai";
+        console.log(arreglo);
+    }
+    if(arreglo[i]=="e"){
+        arreglo[i]="enter";
+    }
+    if(arreglo[i]=="i"){
+        arreglo[i]="imes";
+        console.log(arreglo);
+    }
+    if(arreglo[i]=="o"){
+        arreglo[i]="ober";
+        console.log(arreglo);
+    }
+    if(arreglo[i]=="u"){
+        arreglo[i]="ufat";
+        console.log(arreglo);
+    }
+
+}
+
+
+for (let i = 0; i < arreglo.length; i++) {
+    if(arreglo[i]=="ai"){
+        arreglo[i]="a";
+        console.log(arreglo);
+    }
+    if(arreglo[i]=="enter"){
+        arreglo[i]="e";
+    }
+    if(arreglo[i]=="imes"){
+        arreglo[i]="i";
+        console.log(arreglo);
+    }
+    if(arreglo[i]=="ober"){
+        arreglo[i]="o";
+        console.log(arreglo);
+    }
+    if(arreglo[i]=="ufta"){
+        arreglo[i]="u";
+        console.log(arreglo);
+    }
+
+}
